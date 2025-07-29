52d0416e8145d14a15fe15c2c73609553182118a
Zahid Ahmed <zahd87956@gmail.com>
2025-07-29 14:58:38 +0530

    Added Facebook Pixel to layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
-import Script from "next/script"; // ✅ Required for GA
+import Script from "next/script";
 
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] });
 
@@ -34,6 +34,34 @@ export default function RootLayout({ children }) {
               `,
             }}
           />
+
+          {/* ✅ Facebook Pixel Script */}
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '4828999520519913');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=4828999520519913&ev=PageView&noscript=1"
+            />
+          </noscript>
         </head>
         <body className={`${outfit.className} antialiased text-gray-700`}>
           <Toaster />
